d3b5c5ca46d30e799747ec05f928933224f61af8
deribew <deribewsoftware@gmail.com>
2024-02-03 11:26:03 +0300

    updated

--- components/Card/index.vue
+++ components/Card/index.vue
@@ -1,7 +1,22 @@
 <template>
-  <div class="border-2 border-gray-200 dark:border-gray-800 rounded-lg  m-2 flex-col dark:hover:bg-gray-800 transition duration-350">
-    <div>
+  <div class="group relative border-2 border-gray-200 dark:border-gray-800 rounded-lg z-50 m-2 flex-col dark:hover:bg-gray-800 overflow-hidden">
+    <div class="flex gap-10">
+      <div class=""><img src="../../public/banner2.png"/></div>
+      <div class="flex flex-col gap-2">
+        <h1>Coca-Cola Family Trivia Challenge App</h1>
+        <p>A beautifully designed application to connect family
+           member while having fun at Chinese Holiday. Has advanced
+            face detection features to allow users easily interact and 
+            respond to questions by their movement. Funded by CocaCola</p>
+            <Tools/>
+      </div>
+    </div>
+
+    <div class="absolute top-0 bottom-0 left-0 right-0 opacity-50 bg-teal-700 w-0 h-full z-0    group-hover:opacity-100 group-hover:w-full     transition-width duration-500 ease-in-out ">
+
+      <div>
       <img src="../../public/banner2.png"/>
+    </div>
     </div>
     
   </div>
---
+++ components/Tools.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col gap-2">
+    <div class="">
+      <img src="../public/react.svg" alt="">
+    </div>
+    <p>React</p>
+    
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
